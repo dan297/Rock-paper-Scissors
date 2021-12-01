645d89fbd4ed8b57708197eeb74bb1800f531042
Dan <Danielwhite98@icloud.com>
2021-12-01 22:42:53 +0000

    if statement

--- script.js
+++ script.js
@@ -6,6 +6,26 @@ var options = ["R", "P", "S"];
 
 function playGame() {
   var play = window.prompt("Please enter R, P, S");
+  if (!play) {
+    window.prompt("goodbye");
+  }
+}
+
+var index = math.floor(math.random() * options.length);
+var computer = options[index];
+
+windows.alert(computer);
+
+if (play === computer) {
+  windows.alert("Its a tie");
+} else if (
+  (play === "R" && computer === "S") ||
+  (play === "P" && computer === "R") ||
+  (play === "S" && computer === "P")
+) {
+  windows.alert("you win");
+} else {
+  windows.alert("you lost");
 }
 
 playGame();
