a9365bd9d5379f07cb8f68a439fbc968515b0169
Dan <Danielwhite98@icloud.com>
2021-12-01 22:45:18 +0000

    final commit

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
-var wins = "Wins";
-var Tie = "Draw";
-var Lost = "Lost";
+var wins = 0;
+var ties = 0;
+var losses = 0;
 
+// Array of options for computer to pick from
 var options = ["R", "P", "S"];
 
-function playGame() {
-  var play = window.prompt("Please enter R, P, S");
-  if (!play) {
-    window.prompt("goodbye");
+var playGame = function () {
+  // Ask user for their choice
+  var userChoice = window.prompt("Enter R, P, or S:");
+
+  // If user pressed Cancel, immediately end function
+  if (!userChoice) {
+    return;
   }
-}
 
-var index = math.floor(math.random() * options.length);
-var computer = options[index];
+  // Convert to uppercase to make comparisons easier
+  userChoice = userChoice.toUpperCase();
 
-windows.alert(computer);
+  // Get random index from array of options
+  var index = Math.floor(Math.random() * options.length);
+  var computerChoice = options[index];
 
-if (play === computer) {
-  windows.alert("Its a tie");
-} else if (
-  (play === "R" && computer === "S") ||
-  (play === "P" && computer === "R") ||
-  (play === "S" && computer === "P")
-) {
-  windows.alert("you win");
-} else {
-  windows.alert("you lost");
-}
+  window.alert("The computer chose " + computerChoice);
 
+  // If choices are the same, it's a tie
+  if (userChoice === computerChoice) {
+    ties++;
+    window.alert("It's a tie!");
+
+    // Check every win condition for the player
+  } else if (
+    (userChoice === "R" && computerChoice === "S") ||
+    (userChoice === "P" && computerChoice === "R") ||
+    (userChoice === "S" && computerChoice === "P")
+  ) {
+    wins++;
+    window.alert("You win!");
+
+    // If above conditions failed, assume player lost
+  } else {
+    losses++;
+    window.alert("You lost!");
+  }
+
+  // Print stats with line breaks
+  window.alert(
+    "Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties
+  );
+
+  // Ask user to play again
+  var playAgain = window.confirm("Play again?");
+
+  // If user pressed OK, run the function again
+  if (playAgain) {
+    playGame();
+  }
+};
+
+// Run the game for the first time
 playGame();
